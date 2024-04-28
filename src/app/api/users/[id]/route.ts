3d901c7fac8659dfce4818879ecb5e18386df3e7
chandratand <Chandra.tand@gmail.com>
2024-04-28 16:52:55 +0700

    feat(api): add CRUD operations for user model

---
+++ src/app/api/users/[id]/route.ts
@@ -0,0 +1,62 @@
+import { db } from '@/db';
+import { NotFoundError } from '@/lib/errors';
+import { errorHandler } from '@/lib/errors/handler';
+import { EditUserValidator } from '@/lib/validators/user';
+
+export async function GET(req: Request, { params }: { params: { id: string } }) {
+  try {
+    const { id } = params;
+    if (!Number(id)) throw new NotFoundError('User not found');
+
+    const user = await db.user.findUnique({
+      where: {
+        id: Number(id),
+        deletedAt: null,
+      },
+    });
+
+    if (!user) throw new NotFoundError('User not found');
+    return Response.json({ code: 200, message: 'User updated successfully.', data: user });
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
+
+export async function PUT(req: Request, { params }: { params: { id: string } }) {
+  try {
+    const { id } = params;
+    if (!Number(id)) throw new NotFoundError('User not found');
+
+    const body = await req.json();
+    const parsedBody = EditUserValidator.parse(body);
+
+    const updatedUser = await db.user.update({
+      where: {
+        id: Number(id),
+        deletedAt: null,
+      },
+      data: parsedBody,
+    });
+    return Response.json({ code: 200, message: 'User details updated successfully.', data: updatedUser });
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
+
+export async function DELETE(req: Request, { params }: { params: { id: string } }) {
+  try {
+    const { id } = params;
+    if (!Number(id)) throw new NotFoundError('User not found');
+
+    const user = await db.user.delete({
+      where: {
+        id: Number(id),
+        deletedAt: null,
+      },
+    });
+
+    return Response.json({ code: 200, message: 'User deleted successfully.' });
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,3 +1,55 @@
-export async function GET() {
-  return Response.json({ message: 'User data retrieved successfully' });
+import { db } from '@/db';
+import { errorHandler } from '@/lib/errors/handler';
+import { CreateUserValidator } from '@/lib/validators/user';
+import { z } from 'zod';
+
+const searchParamsSchema = z.object({
+  take: z
+    .string()
+    .refine((val) => Number(val) >= 0, {
+      message: `The 'take' parameter must be a positive number.`,
+    })
+    .optional(),
+  skip: z
+    .string()
+    .refine((val) => Number(val) >= 0, {
+      message: `The 'skip' parameter must be a positive number.`,
+    })
+    .optional(),
+  search: z.string().default(''),
+});
+
+export async function GET(req: Request) {
+  try {
+    const searchParams = Object.fromEntries(new URL(req.url).searchParams.entries());
+    const { take, skip, search } = searchParamsSchema.parse(searchParams);
+
+    const users = await db.user.findMany({
+      where: {
+        deletedAt: null,
+        OR: [{ name: { contains: search } }, { email: { contains: search } }, { address: { contains: search } }],
+      },
+      take: Number(take) || undefined,
+      skip: Number(skip) || undefined,
+    });
+
+    return Response.json({ code: 200, message: 'User data retrieved successfully', data: users });
+  } catch (error) {
+    return errorHandler(error);
+  }
+}
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const parsedBody = CreateUserValidator.parse(body);
+    const createdUser = await db.user.create({
+      data: parsedBody,
+    });
+
+    return Response.json({ code: 201, message: 'User created successfully', data: createdUser }, { status: 201 });
+  } catch (error) {
+    return errorHandler(error);
+  }
 }
